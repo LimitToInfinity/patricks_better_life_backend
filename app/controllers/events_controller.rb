bcb97cffc0add016f021199c90d41dfb124c4bed
LimitToInfinity <21stLimitless@gmail.com>
2020-01-13 16:01:21 -0700

    Adds update method and route to events.

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,4 +1,5 @@
 class EventsController < ApplicationController
+    before_action :find_event, only: [:update, :destroy]
     
     def index
         @events = Event.all
@@ -11,17 +12,25 @@ class EventsController < ApplicationController
         render json: EventSerializer.new(@event)
     end
 
-    def destroy
-        @event = Event.find(params[:id])
-        @event.destroy
+    def update
+        @event.update(event_params)
+
+        render json: EventSerializer.new(@event)
     end
 
+    def destroy
+        @event.destroy
+    end
+    
     private
-
+    
     def event_params
         params.require(:event).permit(:name, :description, 
             :datetime, :user
         ).merge(user: User.find(1))
     end
-
+    
+    def find_event
+        @event = Event.find(params[:id])
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :users, only: [:index]
-  resources :events, only: [:index, :create, :destroy]
+  resources :events, only: [:index, :create, :update, :destroy]
 
   get "/", to: "events#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
