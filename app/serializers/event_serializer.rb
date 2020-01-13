582dc51a278d96dc76ad6b43ed9dbc6860c4c887
LimitToInfinity <21stLimitless@gmail.com>
2020-01-13 14:14:53 -0700

    Modifies event serializer.

--- app/serializers/event_serializer.rb
+++ app/serializers/event_serializer.rb
@@ -1,9 +1,13 @@
 class EventSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :id, :name, :description, :host, :users
+  attributes :id, :name, :description, :host, :attendees
 
   attribute :host do |event|
     event.user
+  end
+
+  attribute :attendees do |event|
+    event.users
   end
 
 end
